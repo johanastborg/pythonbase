a73d5e9e60c1553c0d082837526ab50471507f9c
Johan Astborg <johan.astborg@innovafintech.com>
2021-03-24 13:33:59 +0100

    working code

---
+++ tests.py
@@ -0,0 +1,22 @@
+# tests.py
+
+import random
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+class SimpleTest(unittest.TestCase):
+
+    def test_pass1(self):
+        self.assertEqual(10, 7 + 3)
+
+    def test_pass2(self):
+        self.assertEqual(100, 10 * 10)
+
+    #def test_fail1(self):
+    #    self.assertEqual(11, 7 + 3)
+
+if __name__ == '__main__':
+    import xmlrunner
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
